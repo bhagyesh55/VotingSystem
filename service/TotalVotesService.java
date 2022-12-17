c585a6afa76a69b4903cb67cbfb68092c887e785
bhagyesh55 <vaidyabhagyesh@gmail.com>
2022-12-17 21:45:21 +0530

    fourth

--- service/TotalVotesService.java
+++
@@ -1,9 +0,0 @@
-package com.example.practice.service;
-
-import java.util.List;
-
-import com.example.practice.entity.TotalVotes;
-
-public interface TotalVotesService {
-	List<TotalVotes> getTotalVotes();
-}
--- votingRepository/TotalVotesRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.practice.votingRepository;
-
-import org.springframework.data.repository.CrudRepository;
-
-import com.example.practice.entity.TotalVotes;
-
-public interface TotalVotesRepository extends CrudRepository<TotalVotes, Integer>{
-
-}
